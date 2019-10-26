26b5c84e4c71014df984bb2576c4dc8295ec3f40
Polina Skoraya <skorayapolina@gmail.com>
2019-10-26 08:33:15 +0300

    form layout

---
+++ main.js
@@ -0,0 +1,19 @@
+/*function openDialog() {
+    let dialog = document.getElementById('dialog-review');
+    dialog.showModal();
+}*/
+/*let dialog = document.querySelector('dialog');
+document.querySelector('#openDialog').onclick = function() {
+    dialog.show(); // Показываем диалоговое окно
+}
+document.querySelector('#closeDialog').onclick = function() {
+    dialog.close(); // Прячем диалоговое окно
+}*/
+let x = document.getElementById("review");
+function showDialog() {
+    x.show();
+}
+function closeDialog() {
+    x.close();
+}
+
